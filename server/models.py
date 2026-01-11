4807a5f8d909f7fdc300848a0f2edff14f3a6ebf
Mr-Dan-kibet <kibetdan56@gmail.com>
2026-01-12 00:44:01 +0300

    adds relationships etween tabes

--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import  relationship
 
 metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
@@ -14,6 +16,12 @@ class Hero(db.Model):
     name = db.Column(db.String)
     super_name = db.Column(db.String)
 
+    # defines relationship between heros and hero_powers tables
+    hero_powers = relationship("HeroPower", back_populates="hero", cascade="all, delete-orphan")
+
+    # defines association proxy beween heros and powers tables
+    powers = association_proxy("hero_powers", "power")
+
 class Power(db.Model):
 
     __tablename__ = 'powers'
@@ -22,7 +30,15 @@ class Power(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
 
-class Hero_power(db.Model):
+    # defines relationship between powers and hero_powers tables
+    hero_powers = relationship("HeroPower", back_populates="power", cascade="all, delete-orphan")
+
+    # defines association proxy beween heros and powers tables
+    heroes = association_proxy("hero_powers", "hero")
+
+
+
+class HeroPower(db.Model):
 
     __tablename__ = 'hero_powers'
 
@@ -30,6 +46,10 @@ class Hero_power(db.Model):
     strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey("heros.id"))
     power_id =  db.Column(db.Integer, db.ForeignKey("powers.id"))
+
+    # defines population relationship between her0_powers and all other tables
+    hero = relationship("Hero", back_populates='hero_powers')
+    power = relationship("Power", back_populates='hero_powers')
 
 
     
